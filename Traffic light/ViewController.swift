48fb847d8cfe1cee52a5e31f9e3d07111b054f13
Ablogin Egor <47031969+egor2810@users.noreply.github.com>
2023-11-03 10:10:56 +0300

    Code optimized

--- Traffic light/ViewController.swift
+++ Traffic light/ViewController.swift
@@ -17,31 +17,31 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        configuratingUI()
+        settingUI()
     }
     
-    private func configuratingUI() {
+    private func settingUI() {
         colors = trafficLightStackView.subviews
         colors.forEach { $0.layer.cornerRadius = $0.layer.frame.width / 2}
         trafficLightButton.layer.cornerRadius = 15
     }
     
     @IBAction func toggleLightButton(_ sender: UIButton) {
-        trafficLightButton.setTitle("Turn on next light", for: .normal)
-        
-        let currentLightIndex = colors.firstIndex(where: { $0.alpha == 1 })
-        
-        colors.forEach { $0.alpha = 0.5 }
-        
-        if let currentLightIndex {
-            if currentLightIndex == colors.count - 1 {
-                colors[0].alpha = 1
-            } else {
-                colors[currentLightIndex + 1].alpha = 1
-            }
-        } else {
+        trafficLightButton.setTitle("Turn on the next light", for: .normal)
+
+        guard let currentLightIndex = colors.firstIndex(where: {
+            $0.alpha == 1
+        }) else {
             colors[0].alpha = 1
+            return
+        }
+        
+        colors.forEach { $0.alpha = 0.3 }
+        
+        if currentLightIndex == colors.endIndex - 1 {
+            colors[0].alpha = 1
+        } else {
+            colors[currentLightIndex + 1].alpha = 1
         }
     }
 }
